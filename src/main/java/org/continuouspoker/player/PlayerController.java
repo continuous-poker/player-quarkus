9955ab76f894da9c3c10f02c5ec112b4655c1bfe
Jan Kreutzfeld <jan.kreutzfeld@doubleslash.de>
2022-12-31 17:06:02 +0100

    Rename package and generate model classes from OpenAPI spec

--- src/main/java/org/continuouspoker/player/PlayerController.java
+++ src/main/java/org/continuouspoker/player/PlayerController.java
@@ -1,4 +1,4 @@
-package de.doubleslash.poker.player;
+package org.continuouspoker.player;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -6,16 +6,17 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import de.doubleslash.poker.player.data.Table;
-import de.doubleslash.poker.player.logic.Strategy;
+import org.continuouspoker.player.model.Bet;
+import org.continuouspoker.player.model.Table;
+import org.continuouspoker.player.logic.Strategy;
 
 @Path("/")
 public class PlayerController {
 
    @POST
    @Consumes(MediaType.APPLICATION_JSON)
-   @Produces(MediaType.TEXT_PLAIN)
-   public int action(final Table table) {
+   @Produces(MediaType.APPLICATION_JSON)
+   public Bet action(final Table table) {
       final Strategy player = new Strategy();
       return player.decide(table);
    }
