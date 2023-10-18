a38bad615f8e98696e840b60ad5893b877eaddfa
Jan Kreutzfeld <jan.kreutzfeld@gmail.com>
2023-10-18 21:50:15 +0200

    Update dependencies, enable dependabot

--- src/main/java/org/continuouspoker/player/PlayerController.java
+++ src/main/java/org/continuouspoker/player/PlayerController.java
@@ -1,14 +1,13 @@
 package org.continuouspoker.player;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.continuouspoker.player.logic.Strategy;
 import org.continuouspoker.player.model.Bet;
 import org.continuouspoker.player.model.Table;
-import org.continuouspoker.player.logic.Strategy;
 
 @Path("/")
 public class PlayerController {
